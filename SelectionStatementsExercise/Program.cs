23dbfa62623cfe0611f0058172d3da1371564224
jmmascorro <jessemascorro88@gmail.com>
2021-12-29 22:42:12 -0600

    finished exercise

---
+++ SelectionStatementsExercise/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace SelectionStatementsExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Random();
+            var favNumber = r.NextDouble();
+
+            Console.WriteLine("Try to guess my favorite number");
+            var userInput = int.Parse(Console.ReadLine());
+
+            if (userInput < favNumber)
+            {
+                Console.WriteLine("Too Low");
+            }
+            else if (userInput > favNumber)
+            {
+                Console.WriteLine("Too High");
+            }
+            else if (userInput == favNumber)
+            {
+                Console.WriteLine("Correct");
+            }
+            else
+            {
+                Console.WriteLine("Nevermind");
+            }
+
+            string subject = "Math";
+
+            Console.WriteLine("What is your favorite subject?");
+            string userSubject = Console.ReadLine();
+
+            switch (userSubject)
+            {
+                case "Math":
+                    Console.WriteLine("Math is the bees knees");
+                    break;
+                case "English":
+                    Console.WriteLine("English is for fools");
+                    break;
+                case "History":
+                    Console.WriteLine("History takes me back");
+                    break;
+                case "Science":
+                    Console.WriteLine("Science is second to Math");
+                    break;
+                default:
+                    Console.WriteLine("Math Rules");
+                    break;
+
+            }
+        }
+    }
+}
